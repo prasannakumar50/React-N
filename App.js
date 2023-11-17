56094cadb903731b49301f2e533dbb6b883a1c01
prasannakumar50 <babblu293@gmail.com>
2023-11-18 01:29:07 +0530

    Lesson1

---
+++ App.js
@@ -0,0 +1,29 @@
+/*]
+        const heading= React.createElement("h1",{id:"XYZ"},"Hello world from RJs!");
+
+
+        console.log(heading);
+       
+
+        const root=ReactDOM.createRoot(document.getElementById("root"));
+
+        root.render(heading);
+*/
+
+
+
+const parent = React.createElement("div", {id:"parent"},
+       
+        React.createElement("div", {id: "child"},[
+            React.createElement("h1", {},"Iam h1 tag"),
+            React.createElement("h2", {},"Iam h2 tag"),
+        ])
+
+     
+       );
+       console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+
